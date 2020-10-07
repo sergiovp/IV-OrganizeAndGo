ef260c079b6391d326e2703b2a83c947dc8a6c53
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2020-10-07 18:40:50 +0200

    Añadida clase "madre" del proyecto, close #22

--- app/organizeandgo/organizeandgo.js
+++ app/organizeandgo/organizeandgo.js
@@ -1,22 +1,14 @@
-class OrganizeAndGo {
+class organize_and_go {
+    constructor(equipos) {
+        this.equipos = equipos;
+    }
+
+    get equipos() {
+        return this.equipos;
+    }
+
+    get equipo(index) {
+        return this.equipos[index];
+    }
     
-    constructor (tareaActual, tiempoEstimado, tareasFuturas, prioridadTarea) {
-        
-    }
-
-    getTareaActual(idPersona) {
-        
-    } 
-
-    getTiempoEstimado(tareaActual) {
-        
-    }
-
-    getTareasFuturas(idPersona) {
-        
-    }
-
-    getProridadTarea(idTarea) {
-        
-    }
 }
