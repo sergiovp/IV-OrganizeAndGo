afc44cffbb7d237732931b505a0834cfb9b540d4
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2020-10-14 19:55:32 +0200

    Añadido fichero para pruebas a modo de "main"

--- app/index.ts
+++ app/index.ts
@@ -3,3 +3,4 @@ import { Empleado } from './organizeandgo/empleado';
 import { Equipo } from './organizeandgo/equipo';
 import { OrganizeAndGo } from './organizeandgo/organizeandgo';
 
+console.log("Hola chic@s");
