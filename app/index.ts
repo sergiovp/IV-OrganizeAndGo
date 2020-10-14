78d651c0d31db696322433a678e730d965915e06
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2020-10-14 19:44:58 +0200

    Añadido fichero index con los includes

---
+++ app/index.ts
@@ -0,0 +1,5 @@
+import { Tarea } from './organizeandgo/tarea';
+import { Empleado } from './organizeandgo/empleado';
+import { Equipo } from './organizeandgo/equipo';
+import { OrganizeAndGo } from './organizeandgo/organizeandgo';
+
