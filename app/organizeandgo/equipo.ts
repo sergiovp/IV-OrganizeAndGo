473ec41624e74bb071ab82f1ae21e35b58021109
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2020-10-18 00:39:11 +0200

    Completada la clase Equipo, close #33 close #34

--- app/organizeandgo/equipo.ts
+++ app/organizeandgo/equipo.ts
@@ -1,5 +1,9 @@
 import { Empleado } from './empleado';
-import { Tarea } from './tarea';
+import { Tarea, Prioridad } from './tarea';
+
+/**
+ * Clase equipo. Un objeto de esta clase representa a un equipo que usa la aplicación.
+ */
 
 class Equipo {
     private _id: number;
@@ -61,6 +65,20 @@ class Equipo {
 
     setTarea(index: number, nuevaTarea: Tarea) {
         this.tareas[index] = nuevaTarea;
+    }
+
+    addEmpleado(id: number, nombre: string, apellido: string, email: string) {
+        let nuevoEmpleado: Empleado = new Empleado(id, nombre, apellido, email);
+        
+        this._empleados.push(nuevoEmpleado);
+    }
+
+    addTarea(id: number, terminada: boolean = false,descripcion: string, tiempoEstimado: string,
+        prioridad: Prioridad, empleadoAsignado: number) {
+        let nuevaTarea: Tarea = new Tarea(id, terminada,descripcion, tiempoEstimado,
+            prioridad, empleadoAsignado);
+
+        this._tareas.push(nuevaTarea);
     }
 }
 
