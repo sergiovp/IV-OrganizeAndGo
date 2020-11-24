a88efba49279f13e0003fdd6df05a1bcd2bb293e
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2020-11-24 18:33:23 +0100

    Prueba info autor

--- functions/info_autor.js
+++ functions/info_autor.js
@@ -1,6 +1,22 @@
+const informacion = require('./datos_autor.json');
+
 exports.handler = async function(event, context) {
-    return {
+    let text = req.body.message.text;
+    let mostrar = "";
+    let chatID = req.body.message.chat.id
+
+    if(text == "/nombre") {
+        mostrar = "Sergio\n";
+    } else {
+        mostrar = "Puedes consultar...\n"
+    }
+
+    let objetoJSON ={ text : mostrar, method : "sendMessage", chat_id : chatID}
+    res.setHeader("Content-Type","application/json");
+    res.status(200).json(objetoJSON)
+
+    /*return {
         statusCode: 200,
-        body: JSON.stringify({message: "Hello World"})
-    };
+        body: JSON.stringify(informacion)
+    };*/
 }
