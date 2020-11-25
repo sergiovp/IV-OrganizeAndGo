ba3b9b3c74fbb97b41747706ecc332750f4ce8cf
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2020-11-25 17:26:46 +0100

    Otra prueba

---
+++ functions/info_autor.js
@@ -0,0 +1,39 @@
+
+exports.handler = async event => {
+    var body = JSON.parse(event.body);
+
+    if(body.message != undefined){
+        var ChatID = body.message.chat.id;
+        var mensajeChat = body.message.text;
+        var respuesta;
+
+        if (mensajeChat == "/start") {
+            respuesta = "¡Hola!, aquí podrás consultar toda la información del autor de OrganizeAndGo.";
+
+        } else if(mensaje == "/autor") {
+            respuesta = "Sergio";
+
+        } else if (mensaje == "/help") {
+            respuesta = "Mensaje de ayuda";
+
+        }else{
+            respuesta = "Escribe /start";
+        }
+
+        return {
+        statusCode: 200,
+        body: JSON.stringify({text: respuesta, method:'sendMessage', chat_id: ChatID}),
+        headers:{
+            'Content-Type': 'application/json'
+            }
+        }
+    }
+    else {
+        respuesta = "Hola";
+        
+        return {
+            statusCode: 200,
+            body: respuesta.toString()
+        }
+    }
+}
