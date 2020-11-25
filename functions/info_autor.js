ba3d803b5add5660846747db300c6febf1966ae3
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2020-11-25 18:01:14 +0100

    Otra prueba

--- functions/info_autor.js
+++ functions/info_autor.js
@@ -7,24 +7,32 @@ exports.handler = async event => {
         var mensajeChat = body.message.text;
         var respuesta;
 
-        if (mensajeChat == "/start") {
-            respuesta = "¡Hola!, aquí podrás consultar toda la información del autor de OrganizeAndGo.";
+        if (mensajeChat == "/start" || mensajeChat == "" || mensajeChat == "/help") {
+            respuesta = "¡Hola!, aquí podrás consultar toda la \
+                información del autor de OrganizeAndGo.\n \
+                Para sonsultar toda la información: /info\n \
+                Para consultar el nombre: /nombre\n \
+                Para consultar el correo: /correo\n \
+                Para consultar el repositorio: /repo\n \
+                Para consultar el GitHub: /github";
 
-        } else if(mensajeChat == "/autor") {
+        } else if(mensajeChat == "/nombre") {
             respuesta = "Sergio Vela";
 
-        } else if (mensajeChat == "/help") {
-            respuesta = "Mensaje de ayuda";
-        }
-        else {
-            respuesta = "Escribe /start";
+        } else if (mensajeChat == "/correo") {
+            respuesta = "sergiovp96@gmail.com";
+
+        } else if (mensajeChat == "/repo") {
+            respuesta = "https://github.com/sergiovp/IV-OrganizeAndGo";
+        } else if (mensajeChat == "/github") {
+            respuesta = "https://github.com/sergiovp";
         }
 
         return {
-        statusCode: 200,
-        body: JSON.stringify({text: respuesta, method:'sendMessage', chat_id: ChatID}),
-        headers:{
-            'Content-Type': 'application/json'
+            statusCode: 200,
+            body: JSON.stringify({text: respuesta, method:'sendMessage', chat_id: ChatID}),
+            headers:{
+                'Content-Type': 'application/json'
             }
         }
     //}
