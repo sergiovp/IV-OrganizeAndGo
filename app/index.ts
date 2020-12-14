3752d4dea28103974f9bf0e04110dcc3ea171457
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2020-12-14 19:26:19 +0100

    Eliminado fichero index con extensión TS

--- app/index.ts
+++
@@ -1,6 +0,0 @@
-import { Tarea } from './organizeandgo/tarea';
-import { Empleado } from './organizeandgo/empleado';
-import { Equipo } from './organizeandgo/equipo';
-import { OrganizeAndGo } from './organizeandgo/organizeandgo';
-
-console.log("Hola chic@s");
