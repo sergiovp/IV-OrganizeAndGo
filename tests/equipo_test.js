32bafd7ea0cb280cf44ea074b27f4e1c481daf17
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2020-12-15 17:48:41 +0100

    Adaptados los tests de la clase Equipo al nuevo diseño de la clase

--- tests/equipo_test.js
+++ tests/equipo_test.js
@@ -4,15 +4,9 @@ const Tarea = require('../app/organizeandgo/tarea');
 const Empleado = require('../app/organizeandgo/empleado');
 
 describe('Testing clase Equipo: ', () => {
-    let empleado1 = new Empleado(1, "Sergio", "Vela", "sergiovp96@gmail.com");
-    let empleado2 = new Empleado(2, "Pepito", "Remix", "remix@hotmail.com");
-    let empleados = [empleado1, empleado2];
-
-    let tarea1 = new Tarea(1, false, "Terminar los tests", "media hora", "Importante", 1);
-    let tarea2 = new Tarea(2, false, "Hacer un commit", "1 segundo", "Poco importante", 2);
-    let tareas = [tarea1, tarea2];
-
-    let unEquipo = new Equipo(1, "Equipo de Vela", empleados, tareas);
+    let unEquipo = new Equipo(1, "Equipo de Vela");
+    unEquipo.addEmpleado(1, "Sergio", "Vela", "sergiovp96@gmail.com");
+    unEquipo.addTarea(1, false, "Una tarea", "1 hora", "Difícil", 1);
 
     it ('Comprobamos el constructor', () => {
         expect(unEquipo).to.be.an('object').and.not.empty;
@@ -27,21 +21,19 @@ describe('Testing clase Equipo: ', () => {
     });
 
     it ('get empleados nos debe devolver un array de empleados', () => {
-        expect(unEquipo.empleados).to.equal(empleados).and.to.be.an('array');
+        expect(unEquipo.empleados).to.be.an('array');
     });
 
     it ('getEmpleado nos debe devolver un empleado en concreto', () => {
-        expect(unEquipo.getEmpleado(0)).to.equal(empleado1).and.to.be.an('object');
-        expect(unEquipo.getEmpleado(1)).to.equal(empleado2).and.to.be.an('object');
+        expect(unEquipo.getEmpleado(0)).to.be.an('object');
     });
 
     it ('get tareas nos debe devolver un array de tareas', () => {
-        expect(unEquipo.tareas).to.equal(tareas).and.to.be.an('array');
+        expect(unEquipo.tareas).to.be.an('array');
     });
 
     it ('getTarea nos debe devolver una tarea en concreto', () => {
-        expect(unEquipo.getTarea(0)).to.equal(tarea1).and.to.be.an('object');
-        expect(unEquipo.getTarea(1)).to.equal(tarea2).and.to.be.an('object');
+        expect(unEquipo.getTarea(0)).to.be.an('object');
     });
 
     it ('set id debe modificar el id del equipo', () => {
@@ -85,24 +77,14 @@ describe('Testing clase Equipo: ', () => {
     });
 
     it ('addEmpleado debe añadir un empleado a la lista de empleados', () => {
-        let nuevoEmpleado = new Empleado(4, "Nombre", "Apellido", "correo@yahoo.com");
-        let nuevoEmpleado2 = new Empleado(5, "Hola", "Adiós", "hola@yahoo.com");
+        unEquipo.addEmpleado(2, "Pepe", "Pepe", "pepe@gmail.com");
 
-        unEquipo.addEmpleado(nuevoEmpleado);
-        unEquipo.addEmpleado(nuevoEmpleado2);
-
-        expect(unEquipo.empleados[1]).to.equal(nuevoEmpleado).and.to.be.an('object');
-        expect(unEquipo.empleados[2]).to.equal(nuevoEmpleado2).and.to.be.an('object');
+        expect(unEquipo.empleados[1]).to.be.an('object');
     });
 
     it ('addTarea debe añadir una tarea a la lista de tareas', () => {
-        let nuevaTarea = new Tarea(27, false, "unatarea", "1h", "Importante", 7);
-        let nuevaTarea2 = new Tarea(70, false, "otratarea", "10h", "Poco importante", 3);
+        unEquipo.addTarea(2, false, "Otra tarea", "1 minuto", "Baja", 2);
 
-        unEquipo.addTarea(nuevaTarea);
-        unEquipo.addTarea(nuevaTarea2);
-
-        expect(unEquipo.tareas[1]).to.equal(nuevaTarea).and.to.be.an('object');
-        expect(unEquipo.tareas[2]).to.equal(nuevaTarea2).and.to.be.an('object');
+        expect(unEquipo.tareas[1]).to.be.an('object');
     });
 });
